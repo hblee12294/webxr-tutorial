76643bf11d698694e2d0ec6c097b4a4502e13244
Hongbin Li <hblee12294@gmail.com>
2024-08-19 01:06:13 -0700

    feat: improve header

--- components/header.tsx
+++ components/header.tsx
@@ -1,51 +1,44 @@
 import Link from "next/link";
 import { Menu } from "lucide-react";
 
-interface Nav {
-  name: string;
-  href: string;
-}
-
-const navs: Nav[] = [
-  {
-    name: "Home",
-    href: "/",
-  },
-  {
-    name: "Demo",
-    href: "/demo",
-  },
-  {
-    name: "Resources",
-    href: "/resources",
-  },
-];
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { NavDesktop, NavMobile } from "@/components/nav";
 
 export function Header() {
   return (
-    <header className="static top-0 z-50 flex-shrink-0 py-4 md:sticky">
-      <div className="container flex flex-col items-start justify-between px-6 mx-auto md:flex-row md:items-center">
-        <Link
-          href="/"
-          className="flex items-center gap-2 text-l font-bold md:text-xl"
-        >
+    <header className="static top-0 py-4 md:sticky">
+      <div className="container flex flex-row items-center justify-between px-6">
+        <Link href="/" className="text-l font-bold md:text-xl">
           WebXR Tutorial
         </Link>
 
-        <nav className="grid grid-flow-col gap-4 mx-auto mt-6 md:mt-0 auto-cols-auto md:auto-rows-auto md:gap-8 lg:gap-12">
-          {navs.map(({ name, href }) => (
-            <Link
-              key={href}
-              href={href}
-              className="text-foreground py-4 transition-colors hover:text-foreground"
-            >
-              {name}
-            </Link>
-          ))}
-        </nav>
+        <div className="hidden md:block mx-auto">
+          <NavDesktop></NavDesktop>
+        </div>
 
         <div>
-          <Menu></Menu>
+          <div className="md:hidden">
+            <Sheet>
+              <SheetTrigger>
+                <Menu size="24" />
+                <span className="sr-only">Toggle navigation menu</span>
+              </SheetTrigger>
+
+              <SheetContent>
+                <SheetHeader>
+                  <SheetDescription>
+                    <NavMobile></NavMobile>
+                  </SheetDescription>
+                </SheetHeader>
+              </SheetContent>
+            </Sheet>
+          </div>
         </div>
       </div>
     </header>
---
+++ components/nav.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { cn } from "@/lib/utils";
+
+interface Nav {
+  name: string;
+  href: string;
+}
+
+const navs: Nav[] = [
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "Demo",
+    href: "/demo",
+  },
+  {
+    name: "Resources",
+    href: "/resources",
+  },
+];
+
+export function NavDesktop() {
+  const pathname = usePathname();
+
+  return (
+    <nav className="grid grid-flow-col gap-4 md:gap-8 lg:gap-12">
+      {navs.map(({ name, href }) => (
+        <Link
+          key={href}
+          href={href}
+          className={cn(
+            "text-foreground py-4 transition-colors hover:text-foreground",
+            pathname === href && "font-semibold",
+          )}
+        >
+          {name}
+        </Link>
+      ))}
+    </nav>
+  );
+}
+
+export function NavMobile() {
+  const pathname = usePathname();
+
+  return (
+    <nav className="grid gap-6 text-lg">
+      {navs.map(({ name, href }) => (
+        <Link
+          key={href}
+          href={href}
+          className={cn(
+            "text-left text-foreground",
+            pathname === href && "font-semibold",
+          )}
+        >
+          {name}
+        </Link>
+      ))}
+    </nav>
+  );
+}
