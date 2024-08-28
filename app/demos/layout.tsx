5f77b1106d82030b208725e9f681576a206b2179
Hongbin Li <hblee12294@gmail.com>
2024-08-28 01:34:43 -0700

    feat: add back link in nested demo page

--- app/demos/layout.tsx
+++ app/demos/layout.tsx
@@ -1,11 +1,26 @@
+import Link from "next/link";
+import { ChevronLeft } from "lucide-react";
+
 import { Header } from "@/components/header";
+import { Button } from "@/components/ui/button";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col h-dvh">
       <Header></Header>
 
-      <div className="flex-1 min-h-0 overflow-hidden">{children}</div>
+      <div className="relative flex-1 min-h-0 overflow-hidden">
+        <div className="absolute top-2 left-2 lg:top-4 lg:left-4">
+          <Link href="/demos">
+            <Button variant="link">
+              <ChevronLeft></ChevronLeft>
+              Demos
+            </Button>
+          </Link>
+        </div>
+
+        {children}
+      </div>
     </div>
   );
 }
