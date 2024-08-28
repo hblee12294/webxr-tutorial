8cd74fb4774c1b8c7879dd1e4aed23d24f0c021b
Hongbin Li <hblee12294@gmail.com>
2024-08-28 15:43:19 -0700

    feat: add demos covers

--- app/(main)/demos/page.tsx
+++ app/(main)/demos/page.tsx
@@ -1,16 +1,38 @@
 import Link from "next/link";
+import Image from "next/image";
+import { SquareArrowOutUpRight } from "lucide-react";
 
 import { DEMOS } from "@/configs/demos";
 import { isExternalUrl } from "@/lib/isExternalUrl";
 
 export default function Page() {
   return (
-    <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 not-prose">
-      {DEMOS.map(({ title, href }) => {
+    <ul className="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-8 not-prose">
+      {DEMOS.map(({ title, href, cover }) => {
+        const isExternal = isExternalUrl(href);
+
         return (
-          <li key={href} className="">
-            <Link href={href} target={isExternalUrl(href) ? "_blank" : "_self"}>
-              <h2>{title}</h2>
+          <li key={href}>
+            <Link
+              href={href}
+              target={isExternalUrl(href) ? "_blank" : "_self"}
+              className="grid gap-2"
+            >
+              <Image
+                src={cover}
+                width={1280}
+                height={720}
+                alt={title}
+                className="rounded-lg border"
+              />
+
+              <h2 className="text-lg flex gap-2 items-center justify-between">
+                {title}
+
+                {isExternal && (
+                  <SquareArrowOutUpRight size={18}></SquareArrowOutUpRight>
+                )}
+              </h2>
             </Link>
           </li>
         );
--- configs/demos.ts
+++ configs/demos.ts
@@ -2,13 +2,16 @@ export const DEMOS = [
   {
     title: "Three.js Boilerplate",
     href: "/demos/base",
+    cover: "/images/base.png",
   },
   {
     title: "Dragging",
     href: "/demos/dragging",
+    cover: "/images/dragging.png",
   },
   {
     title: "Volumetric Video",
     href: "https://mediax.tencent.com/app/xr",
+    cover: "/images/volumetric.png",
   },
 ];
