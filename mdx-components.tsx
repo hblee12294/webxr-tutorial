238a11d7db51f7af1a50bbf08b9359e8c92a6d1a
Hongbin Li <hblee12294@gmail.com>
2024-08-28 14:49:22 -0700

    feat: update contents

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,9 +1,20 @@
+import Link from "next/link";
 import type { MDXComponents } from "mdx/types";
 
+import { isExternalUrl } from "@/lib/isExternalUrl";
 import { H2 } from "@/components/ui/h2";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    a: ({ href, children }) => {
+      return href ? (
+        <Link href={href} target={isExternalUrl(href) ? "_blank" : "_self"}>
+          {children}
+        </Link>
+      ) : (
+        <a>{children}</a>
+      );
+    },
     h2: ({ children }) => <H2>{children}</H2>,
     ...components,
   };
